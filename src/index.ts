b1148bc80d5fb1a69f1cf4150ff04271aa0740ff
StanislavDevel <unfeel00@gmail.com>
2025-11-05 16:57:16 +0100

    feat(api): add base server, router, and in-memory users database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { createServer } from 'http';
 import dotenv from 'dotenv'
+import { router } from './routers/routers.js';
+import { IncomingMessage, ServerResponse } from 'node:http';
 dotenv.config()
 const PORT = process.env.PORT || 3000;
 
-const server = createServer((req, res) => {
-  res.end('Server is running');
+const server = createServer((req:IncomingMessage, res:ServerResponse) => {
+  if(req.url === undefined) return null
+  router(req,res)
+
 });
 
 server.listen(PORT, () => {
   console.log('Server started on port', PORT);
 });
+
